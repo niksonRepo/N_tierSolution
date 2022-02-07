a54d5ca5e8ee303750479186072187eda15356eb
Nikita Savcenko <102829@norwegian.com>
2022-02-07 20:42:33 +0200

    DateTime

--- N-tierSolution.UI/Program.cs
+++ N-tierSolution.UI/Program.cs
@@ -7,6 +7,13 @@ namespace N_tierSolution.UI
     {
         public static void Main(string[] args)
         {
+            var dateTime = DateTime.Today.ToString();
+            var date = $"{DateTime.Today.Day}" +
+                       $"/{DateTime.Today.Month}" +
+                       $"'/{DateTime.Today.Year}";
+
+            var cretedDate = new DateTime(2022, 09, 12);
+
             var businessLogicObject = new BusinessLogic();
             var getConcatResult = businessLogicObject.ConcatanateString("John", 18);
             var validationResult = businessLogicObject.ValidateAge(11);
